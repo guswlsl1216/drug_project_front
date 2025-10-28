18b34e15208d7c2f00ac3b6f851670a5afcf4a04
ss-oh <tlstmddh12@gmail.com>
2025-10-28 17:51:12 +0900

    feature/mainpage

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,14 +1,15 @@
-import { Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import Mainpage from "./pages/Mainpage";
 
 const Routers = () => {
 
   return (
     <>
       <Routes>
-        
+        <Route path="/" element={<Mainpage />} />
       </Routes>
     </>
-  )
+  );
 }
 
 export default Routers
