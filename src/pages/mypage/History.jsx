ea4fa7ae4bc4695985271dc9c8d0bf52a01811bc
jjop2 <jngsuh616@gmail.com>
2025-11-07 17:56:59 +0900

    Feat: 분석 결과 내역 기능 구현 중

--- src/pages/mypage/History.jsx
+++ src/pages/mypage/History.jsx
@@ -2,119 +2,66 @@ import '../../styles/mypage/History.css';
 import Button from "../../components/ui/Button";
 import requestHandler from '../../utils/requestHandler';
 import { useEffect, useState } from 'react';
+import '../../styles/utils/analysisStatus.css';
+import ANALYSIS_STATUS_MAPPING from '../../utils/analysisStatus';
+import Spinner from '../../components/ui/Spinner';
+import UseNavi from '../../utils/UseNavi';
+import { replace, useLocation } from 'react-router-dom';
 
 const History = () => {
-  const [historyList, setHistoryList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [historyList, setHistoryList] = useState([]); // 분석 결과 목록
 
-  const [currentPage, setCurrentPage] = useState(1);  // 현재 페이지 번호
-  const [startPage, setStartPage] = useState(1);  // 현재 첫 페이지 번호
-  const [endPage, setEndPage] = useState(10);  // 현재 마지막 페이지 번호
-  const pageNumbers = Array.from({ length: endPage }, (_, i) => startPage + i); // 화면에서 보여줄 총 페이지 번호에 따른 배열
+  const [has_prev, setHas_prev] = useState(false);  // 이전 페이지 유무
+  const [has_next, setHas_next] = useState(false);  // 다음 페이지 유무
+  const [pageNumbers, setPageNumbers] = useState(null); // 페이지 버튼에 보여줄 번호 리스트
 
-  const status_color = {
-    '정상' : 'history_normal',
-    '주의' : 'history_warning',
-    '위험' : 'history_danger'
+  const { goTo } = UseNavi();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  let initialPage = parseInt(queryParams.get('page'), 10);
+  
+  if(isNaN(initialPage) || initialPage < 1) {
+    initialPage = 1;
+    goTo(`/mypage/history?page=${initialPage}`, {replace: true});
   }
 
-  // test
-  useEffect(() => {
-    setHistoryList([
-    {
-      status: '주의',
-      analysis_date: '2025.11.01',
-      meds_ingredients: ['약1', '약2', '약3'],
-      supps:[
-        {
-          "name": "홍삼정 에브리타임",
-          "ingredients": ["진세노사이드", "나트륨", "카페인"]
-        },
-        {
-          "name": "오메가3 프리미엄",
-          "ingredients": ["EPA", "DHA", "비타민 E"]
-        },
-      ]
-    },
-    {
-      status: '정상',
-      analysis_date: '2025.10.26',
-      meds_ingredients: ['약1', '약2', '약3'],
-      supps:[
-        {
-          "name": "홍삼정 에브리타임",
-          "ingredients": ["진세노사이드", "나트륨", "카페인"]
-        },
-        {
-          "name": "오메가3 프리미엄",
-          "ingredients": ["EPA", "DHA", "비타민 E"]
-        },
-      ]
-    },
-    {
-      status: '주의',
-      analysis_date: '2025.10.15',
-      meds_ingredients: ['약1', '약2', '약3'],
-      supps:[
-        {
-          "name": "홍삼정 에브리타임",
-          "ingredients": ["진세노사이드", "나트륨", "카페인"]
-        },
-        {
-          "name": "오메가3 프리미엄",
-          "ingredients": ["EPA", "DHA", "비타민 E"]
-        },
-      ]
-    },
-    {
-      status: '위험',
-      analysis_date: '2025.10.08',
-      meds_ingredients: ['약1', '약2', '약3'],
-      supps:[
-        {
-          "name": "홍삼정 에브리타임",
-          "ingredients": ["진세노사이드", "나트륨", "카페인"]
-        },
-        {
-          "name": "오메가3 프리미엄",
-          "ingredients": ["EPA", "DHA", "비타민 E"]
-        },
-      ]
-    }
-  ])
-  }, [currentPage]);
-
-  // useEffect(() => {
-  //   requestHandler({
-  //     method: "get",
-  //     url: "/history",
-  //     params: { page: currentPage, size: 10 },
-  //     setLoading,
-  //     onSuccess: (data) => setHistoryList(data),
-  //     onError: (msg) => alert(msg),
-  //   })
-  // }, []);
-
-  if(loading) return <p>불러오는 중...</p>
-
+  const [currentPage, setCurrentPage] = useState(initialPage);  // 현재 페이지 번호
   
-  function historyCard(history) {
+  useEffect(() => {
+    requestHandler({
+      method: "get",
+      url: "/result/history",
+      params: { page: currentPage },
+      setLoading,
+      onSuccess: (data) => {
+        setHistoryList(data.history);
+        setHas_prev(data.has_prev);
+        setHas_next(data.has_next);
+        setPageNumbers(data.pageNumbers);
+      },
+      onError: (msg) => alert(msg),
+    })
+  }, [currentPage, location.search]);
+  
+  const historyCard = (history) => {
+    const { status_label, status_className } = ANALYSIS_STATUS_MAPPING[history.status]
+    const fullDateTime = history.analysis_date;
+    const dateOnlySlice = fullDateTime.slice(0, 10);
+    
     return (
       <article className="history_card">
         <div className="history_cardInfo">
-          <div className={`history_cardInfo_logo ${status_color[history.status]}`}>
-            <p>{history.status}</p>
+          <div className={`history_cardInfo_logo ${status_className}`}>
+            <p>{status_label}</p>
           </div>
           <div className="history_cardInfo_content">
-            <h3>{history.analysis_date}</h3>
+            <h3>{dateOnlySlice}</h3>
             <p className='ellipsis'><span>[의약품]</span> {
-              history.meds_ingredients.map(med => med).join(', ')
+              history.meds.map(med => med.name).join(', ')
             }</p>
             <p className='ellipsis'><span>[영양제]</span> {
-              history.supps.map(
-                // supp => `${supp.name}(${supp.ingredients.map(ing => ing).join(', ')})` => 성분까지 표시
-                supp => supp.name
-              ).join(', ')
+              history.supps.map(supp => supp.name).join(', ')
             }</p>
           </div>
         </div>
@@ -133,34 +80,67 @@ const History = () => {
             <h2>분석결과내역 페이지</h2>
           </div>
 
-          <ul className="history_cardlist">
-            {
-              historyList.map((history, i) => {
-                return (
-                  <li key={i}>{historyCard(history)}</li>
-                )
-              })
-            }
-          </ul>
+          {
+            loading
+            ?
+            <div className="drug_info_loadingSpinner">
+              <div className="overlay">
+                <Spinner size={70} color="#00e2ff" />
+              </div>
+            </div>
+            :
+            <>
+            <ul className="history_cardlist">
+              {
+                historyList.map((history, i) => {
 
-          <div className="history_pagination">
-            <ul className="history_pagination_list">
-              {
-                
+                  return (
+                    <li key={i}>{historyCard(history)}</li>
+                  )
+                })
               }
-              <li onClick={() => setCurrentPage(currentPage - 10)}>&lt;</li>
-              {
-                pageNumbers.map((page, i) => (
-                  <li
-                    key={i}
-                    className={currentPage == page ? 'history_active' : '' }
-                    onClick={() => setCurrentPage(page)}
-                  >{page}</li>
-                ))
-              }
-              <li onClick={() => setCurrentPage(currentPage + 10)}>&gt;</li>
             </ul>
-          </div>
+
+            <div className="history_pagination">
+              <ul className="history_pagination_list">
+                {
+                  has_prev &&
+                  <li className='history_pagination_prev' onClick={() => {
+                    setCurrentPage(currentPage - 1)
+                    goTo(`/mypage/history?page=${currentPage - 1}`)
+                  }}>&lt;</li>
+                }
+                {
+                  pageNumbers &&
+                  pageNumbers.map((page, i) => {
+                    if(page) {
+                      return (
+                        <li
+                        key={i}
+                        className={currentPage == page ? 'history_active' : '' }
+                        onClick={() => {
+                          setCurrentPage(page)
+                          goTo(`/mypage/history?page=${page}`)
+                        }}
+                        >{page}</li>
+                      )
+                    } else {
+                      return <p key={i}>...</p>
+                    }
+                  })
+                }
+                {
+                  has_next &&
+                  <li className='history_pagination_next' onClick={() => {
+                    setCurrentPage(currentPage + 1)
+                    goTo(`/mypage/history?page=${currentPage + 1}`)
+                  }}>&gt;</li>
+                }
+              </ul>
+            </div>
+            </>
+          }
+
         </section>
       </main>
     </>
