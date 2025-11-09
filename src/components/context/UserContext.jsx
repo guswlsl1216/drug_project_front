97e542eb7eda8c11ee83fb44569e04da4452a135
unknown <thduszbzb12@gmail.com>
2025-11-10 00:23:03 +0900

    Fix:로그인 유지 수정, 로그아웃 수정

--- src/components/context/UserContext.jsx
+++ src/components/context/UserContext.jsx
@@ -1,16 +1,40 @@
-import { children, createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import requestHandler from "../../utils/requestHandler";
+import axiosInstance from "../../utils/axiosInstance";
 
 // Context 생성
-const UserContext = createContext();
+export const UserContext = createContext();
 
 // Provider 생성
 export const UserProvider = ({children}) => {
   const [user, setUser] = useState(null); // 로그인 한 유저 정보
+  const [loading, setLoading] = useState(true); // 로그인 한 유저 정보
 
-  const isLoggedIn = () => !!user;
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        const res = await axiosInstance.get("login/check"); // 쿠키 포한 자동 전송
 
+        if (res.data.logged_in) {
+          setUser(res.data.user); // nickname, id 등 전체 user 정보
+        } else {
+          setUser(null);
+        }
+      } catch (error) {
+        console.error("로그인 상태 확인 실패:", error);
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkLogin();
+  }, []);
+
+  const isLoggedIn = !!user;
+    
   return (
-    <UserContext.Provider value={{ user, setUser, isLoggedIn }}>
+    <UserContext.Provider value={{ user, setUser, isLoggedIn, loading }}>
       {children}
     </UserContext.Provider>
   );
