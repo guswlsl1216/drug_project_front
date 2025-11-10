497fac017744759dc70a4179b6da8f2565eb7043
jjop2 <jngsuh616@gmail.com>
2025-11-10 14:15:20 +0900

    Fix: 분석 결과 중복 저장 방지 수정

--- src/pages/analyze/AnalyzeResult.jsx
+++ src/pages/analyze/AnalyzeResult.jsx
@@ -2,12 +2,14 @@ import '../../styles/analyze/AnalyzeResult.css'
 import AnalyzeResultDisplay from '../../components/analyze/AnalyzeResultDisplay';
 import Button from '../../components/ui/Button'
 import requestHandler from '../../utils/requestHandler';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const AnalyzeResult = () => {
   // 아래 임시 코드들은 분석 기능 구현 완료 후 제거 예정
   // 결과 데이터(임시)
   const result_test = {
+    // 추가 : 중복 저장 방지용 식별코드 (uuid)
+		"analysis_uid": "cc6f72c2-91e8-4ed9-8d6d-6a9f0871a537",
     "status": 2,
     "meds": [
       {
@@ -71,25 +73,45 @@ const AnalyzeResult = () => {
   sessionStorage.setItem('result', JSON.stringify(result_test))
 
   const [loading, setLoading] = useState(false);
-  
+  const [isSave, setIsSave] = useState(false);  // 결과 저장 여부
   
   // 세션스토리지에서 불러오기
   const result = JSON.parse(sessionStorage.getItem('result'))
   
   const saveResult = () => {
-    requestHandler({
-      method: "post",
-      url: "/result/save",
-      payload: result,
-      setLoading,
-      onSuccess: (data) => {
-        alert(data.message);
-      },
-      onError: (msg) => {
-        alert(msg);
-      }
-    })
+    if (sessionStorage.getItem('isSave') === result.analysis_uid) {
+      alert('이미 저장되어 있습니다.');
+      return;
+    } else if (isSave) {
+      alert('저장 중입니다. 잠시만 기다려주세요.');
+      return;
+    } else {
+      requestHandler({
+        method: "post",
+        url: "/result/save",
+        payload: result,
+        setLoading,
+        onSuccess: (data) => {
+          alert(data.message);
+          setIsSave(data.isSave);
+          // 결과 저장 성공 시, 해당 결과 고유 번호가 세션(isSave)에 저장됨
+          // 세션에 저장된 isSave의 값이 현재 결과 고유 번호와 같으면 저장 버튼 비활성화
+          // 만약 테스트를 위해 버튼 활성화가 필요한 경우 세션 삭제 바람
+          sessionStorage.setItem('isSave', result.analysis_uid)
+        },
+        onError: (msg) => {
+          alert(msg);
+          sessionStorage.removeItem('isSave');
+        }
+      })
+    }
   }
+
+  useEffect(() => {
+    if(sessionStorage.getItem('isSave') === result.analysis_uid) {
+      setIsSave(true);
+    }
+  }, [])
   
   return (
     <>
@@ -101,7 +123,7 @@ const AnalyzeResult = () => {
         <AnalyzeResultDisplay result={result} />
 
         <div className='analyze_result_actions'>
-          <Button variant='primary' onClick={saveResult} disabled={loading}>{loading ? '저장 중...' : '결과 저장'}</Button>
+          <Button variant='primary' onClick={saveResult} disabled={loading || isSave}>{(loading ? '저장 중...' : '결과 저장') || (isSave ? '저장 완료' : '결과 저장')}</Button>
           <Button variant='primary' onClick={() => alert('루틴 페이지 이동')}>복용 루틴 설정</Button>
         </div>
       </div>
